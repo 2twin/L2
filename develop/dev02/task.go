92eb3dc1ccc0cc3fd36e75eb27e70aa69d570474
Ilya Vladimirov <vl.ilya@mail.ru>
2024-03-12 21:20:24 +0300

    dev02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -18,6 +18,63 @@ package main
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+var (
+	ErrIncorrectString = errors.New("incorrect string")
+)
+
+func Unzip(s string) (string, error) {
+	if len(s) == 0 {
+		return "", ErrIncorrectString
+	}
+
+	if _, err := strconv.Atoi(s); err == nil {
+		return "", ErrIncorrectString
+	}
+
+	arr := strings.Split(s, "")
+	res := []string{}
+
+	for _, r := range arr {
+		n, err := strconv.Atoi(r)
+		if err != nil {
+			res = append(res, r)
+			continue
+		}
+
+		if len(res) == 0 {
+			break
+		}
+
+		i := len(res) - 1
+		current := res[i]
+
+		if current == `\` {
+			if res[i-1] != `\` {
+				res[i] = r
+				continue
+			}
+
+			i -= 1
+			n -= 1
+		}
+
+		s := strings.Repeat(current, n)
+		res[i] = s
+	}
+
+	return strings.Join(res, ""), nil
+}
+
+func main() {
+	str := []string{"a4bc2d5e", "abcd", "45", "", `qwe\4\5`, `qwe\45`, `qwe\\5`}
+	for _, s := range str {
+		fmt.Println(Unzip(s))
+	}
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"testing"
+)
+
+type Result struct {
+	String string
+	Error  error
+}
+
+func TestUnzip(t *testing.T) {
+	cases := map[string]Result{
+		"a4bc2d5e": {"aaaabccddddde", nil},
+		"abcd":     {"abcd", nil},
+		"45":       {"", ErrIncorrectString},
+		"":         {"", ErrIncorrectString},
+		"5oijsd":   {"", ErrIncorrectString},
+		`qwe\4\5`:  {"qwe45", nil},
+		`qwe\45`:   {"qwe44444", nil},
+		`qwe\\5`:   {`qwe\\\\\`, nil},
+		"$µ2€№3":   {"$µµ€№№№", nil},
+	}
+
+	for in, out := range cases {
+		got, err := Unzip(in)
+
+		if got != out.String && err != out.Error {
+			t.Errorf("Want: %s, got: %s", got, out)
+		}
+	}
+}
